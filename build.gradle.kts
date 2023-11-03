4a5628a7b231ff8434c98ce52cc79546beac4027
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2023-11-02 22:54:23 -0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    kotlin("jvm") version "1.9.20"
+//    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("xyz.jpenilla.run-paper") version "2.2.0"
+}
+
+group = "com.megabyte6"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc-repo"
+    }
+    maven("https://oss.sonatype.org/content/groups/public/") {
+        name = "sonatype"
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+//tasks.shadowJar {
+//    archiveBaseName.set(rootProject.name)
+//    archiveClassifier.set("")
+//}
+
+tasks.runServer {
+    minecraftVersion("1.20.2")
+}
---
+++ src/main/kotlin/com/megabyte6/dojodirector/DojoDirector.kt
@@ -0,0 +1,13 @@
+package com.megabyte6.dojodirector
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DojoDirector : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
