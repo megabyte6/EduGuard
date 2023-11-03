b962558a01ed1bc1340d40b8cb168022adff696e
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2023-11-03 14:01:29 -0700

    Add config.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.20"
-//    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("xyz.jpenilla.run-paper") version "2.2.0"
 }
 
@@ -25,10 +25,18 @@ kotlin {
     jvmToolchain(17)
 }
 
-//tasks.shadowJar {
-//    archiveBaseName.set(rootProject.name)
-//    archiveClassifier.set("")
-//}
+tasks.build {
+    dependsOn("shadowJar")
+}
+
+tasks.shadowJar {
+    archiveBaseName.set(rootProject.name)
+    archiveClassifier.set("")
+//    dependencies {
+//        include(dependency("org.jetbrains.kotlin:kotlin-stdlib:1.4.0"))
+//    }
+//    mergeServiceFiles()
+}
 
 tasks.runServer {
     minecraftVersion("1.20.2")
--- src/main/kotlin/com/megabyte6/dojodirector/DojoDirector.kt
+++ src/main/kotlin/com/megabyte6/dojodirector/DojoDirector.kt
@@ -1,13 +1,20 @@
 package com.megabyte6.dojodirector
 
+import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.plugin.java.JavaPlugin
 
 class DojoDirector : JavaPlugin() {
+
+    companion object {
+        val defaultConfig = YamlConfiguration()
+    }
+
     override fun onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig()
+        defaultConfig.load(javaClass.getResourceAsStream("/config.yml")!!.bufferedReader())
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+        saveConfig()
     }
 }
