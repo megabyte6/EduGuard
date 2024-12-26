db97c99a70021fa7513fc295f9cdc205305e8640
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2024-12-26 01:06:15 -0800

    Feature/hangar publishing (#6)
    * Fix formatting
    
    * Add hangar publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,24 @@
+import io.papermc.hangarpublishplugin.model.Platforms
+
 plugins {
     kotlin("jvm") version "2.+"
     id("com.gradleup.shadow") version "8.+"
     id("com.modrinth.minotaur") version "2.+"
+    id("io.papermc.hangar-publish-plugin") version "0.1.+"
     id("xyz.jpenilla.run-paper") version "2.+"
 }
 
-group = providers.gradleProperty("group").get()
-version = providers.gradleProperty("version").get()
+project.group = providers.gradleProperty("group").get()
+project.version = providers.gradleProperty("version").get()
 
 val minecraftVersion: String by project
 val paperMcVersion: String by project
 
 val modrinthVersionType: String by project
 val modrinthReleaseGameVersions: String by project
+
+val hangarReleaseChannel: String by project
+val hangarReleaseGameVersion: String by project
 
 repositories {
     mavenCentral()
@@ -38,8 +44,23 @@ modrinth {
     projectId.set("ueOBZDS1")
     versionType.set(modrinthVersionType)
     uploadFile.set(tasks.shadowJar)
-    gameVersions.addAll(modrinthReleaseGameVersions.split(","))
+    gameVersions.addAll(modrinthReleaseGameVersions.split(",").map(String::trim))
     syncBodyFrom.set(rootProject.file("README.md").readText())
+}
+
+hangarPublish {
+    publications.register("plugin") {
+        version.set(project.version as String)
+        channel.set(hangarReleaseChannel)
+        id.set("EduGuard")
+        apiKey.set(System.getenv("HANGAR_API_TOKEN"))
+        platforms {
+            register(Platforms.PAPER) {
+                jar.set(tasks.shadowJar.flatMap { it.archiveFile })
+                platformVersions.set(hangarReleaseGameVersion.split(",").map(String::trim))
+            }
+        }
+    }
 }
 
 tasks {
