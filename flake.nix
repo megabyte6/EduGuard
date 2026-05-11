fd06a9094f2674b674ab049ba082c7bd885a4a80
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2026-05-11 09:44:15 -0700

    Add Nix development support with flake configuration (#10)

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "A Nix-flake-based Kotlin development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+
+  outputs = {self, ...} @ inputs: let
+    javaVersion = 25;
+
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (
+        system:
+          f {
+            inherit system;
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [inputs.self.overlays.default];
+            };
+          }
+      );
+  in {
+    overlays.default = final: prev: let
+      jdk = prev."jdk${toString javaVersion}";
+    in {
+      gradle = prev.gradle_9.override {java = jdk;};
+      kotlin = prev.kotlin.override {jre = jdk;};
+    };
+
+    devShells = forEachSupportedSystem (
+      {
+        pkgs,
+        system,
+      }: {
+        default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            gcc
+            gradle
+            kotlin
+            ncurses
+            patchelf
+            zlib
+            self.formatter.${system}
+          ];
+        };
+      }
+    );
+
+    formatter = forEachSupportedSystem ({pkgs, ...}: pkgs.nixfmt);
+  };
+}
